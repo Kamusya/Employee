68c341bf4f2cfab7be883aa7757ec71c38a0d4df
Kamusya <79401271+Kamusya@users.noreply.github.com>
2021-03-10 15:18:29 +0600

    My OOP assignment 4 - company

--- DbConnection/src/com/company/data/PostgresDB.java
+++ DbConnection/src/com/company/data/PostgresDB.java
@@ -8,7 +8,7 @@ public class PostgresDB implements IDB {
     /*to access the interface methods, the interface must be implemented*/
     @Override
     public Connection getConnection() throws SQLException, ClassNotFoundException {
-        String connectionUrl = "jdbc:postgresql://localhost:5432/PostgresDB"; /*name of database*/
+        String connectionUrl = "jdbc:postgresql://localhost:5432/simpleDB"; /*name of database*/
         try {
             Class.forName("org.postgresql.Driver"); /*the driver’s class file*/
             /*Connection con = DriverManager.getConnection(connectionUrl, "postgres", "5678a");*/
